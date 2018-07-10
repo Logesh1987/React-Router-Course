8131ca174509e2cd3eef04aace93e2e72ed5d0a9
Logesh <logesh.kumar@krds.com>
2018-07-10 10:30:03 +0530

    passing props to route

--- app/components/App.js
+++ app/components/App.js
@@ -4,6 +4,7 @@ import {
     Route,
     Link
 } from "react-router-dom";
+var Program = require('./Program');
 
 const topics = [
     {
@@ -128,17 +129,21 @@ const Topics = ({match})=> {
 class App extends React.Component {
     render() {
         return (
-            <Router>
-                <div>
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/topics">Topics</Link></li>
-                    </ul>
-                    <hr/>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/topics" component={Topics} />
-                </div>
-            </Router>
+            <div>
+                <Router>
+                    <div>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/topics">Topics</Link></li>
+                            <li><Link to="program-navigation">Programmatically navigate using React Router</Link></li>
+                        </ul>
+                        <hr/>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/topics" component={Topics} />
+                        <Route path="/program-navigation" render={(props) => <Program init={true} />} />
+                    </div>
+                </Router>
+            </div>
         )
     }
 }
---
+++ app/components/Program.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+class Program extends React.Component {
+    render() {
+        if(init) {
+
+            return (
+                <h1>{init}</h1>
+            )
+        }
+    }
+
+}
+
+module.exports = Program;
